a7c4b7c033a82df99d1f20019201a0648af53dcd
Edwin Velasquez Jimenez <liontumbler@gmail.com>
2024-05-24 06:16:01 -0500

    parte tres

--- index.py
+++ index.py
@@ -1,13 +1,13 @@
-from flask import Flask
-from src.routes.routes import init_routes
-
 import psutil
-import GPUtil
-
 cpu_usage = psutil.cpu_percent(interval=1)
 print(f"Uso de CPU: {cpu_usage}%")
 
+import GPUtil
 gpus = GPUtil.getGPUs()
+print(gpus)
+#gpu = gpus[0]
+#print(gpu)
+"""
 for gpu in gpus:
     print(f"GPU: {gpu.name}")
     print(f"Uso de GPU: {gpu.load * 100}%")
@@ -15,6 +15,10 @@ for gpu in gpus:
     print(f"Memoria utilizada: {gpu.memoryUsed}MB")
     print(f"Memoria total: {gpu.memoryTotal}MB")
     print(f"Temperatura: {gpu.temperature} C")
+"""
+
+from flask import Flask
+from src.routes.routes import init_routes
 
 app = Flask(__name__)
 
